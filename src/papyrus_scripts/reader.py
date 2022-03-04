edeb929bdcaa991307ef3f2c220f33a48564ec19
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2022-03-04 14:21:26 +0100

    Use pystow

--- src/papyrus_scripts/reader.py
+++ src/papyrus_scripts/reader.py
@@ -5,24 +5,31 @@ import json
 import os
 from typing import Optional, Union, Iterator
 
+import pystow
 import pandas as pd
 
 from .utils.IO import TypeDecoder
 
 
-def read_papyrus(is3d: bool = False, chunksize: Optional[int] = None, source_path: str = './') -> Union[
+def read_papyrus(is3d: bool = False, chunksize: Optional[int] = None, source_path: Optional[str] = None) -> Union[
     Iterator[pd.DataFrame], pd.DataFrame]:
     """Read the Papyrus dataset.
 
     :param is3d: whether to consider stereochemistry or not (default: False)
     :param chunksize: number of lines per chunk. To read without chunks, set to None
-    :param source_path: folder containing the bioactivity dataset
+    :param source_path: folder containing the bioactivity dataset (default: pystow's home folder)
     :return: the Papyrus activity dataset
     """
     # Load data types
     dtype_file = os.path.join(os.path.dirname(__file__), 'utils', 'data_types.json')
     with open(dtype_file, 'r') as jsonfile:
         dtypes = json.load(jsonfile, cls=TypeDecoder)['papyrus']
+    # Determine default paths
+    if source_path is not None:
+        os.environ['PYSTOW_HOME'] = os.path.abspath(source_path)
+    source_path = pystow.module('papyrus')
+    source_path = source_path.base.as_posix()
+    # Find the file
     file_mask = os.path.join(source_path, f'*.*_combined_set_with{"out" if not is3d else ""}_stereochemistry.tsv*')
     filename = glob.glob(file_mask)
     # Handle WSL ZoneIdentifier files
@@ -32,13 +39,18 @@ def read_papyrus(is3d: bool = False, chunksize: Optional[int] = None, source_pat
     return pd.read_csv(filename[0], sep='\t', chunksize=chunksize, dtype=dtypes, low_memory=True)
 
 
-def read_protein_set(source_path: str = './') -> pd.DataFrame:
+def read_protein_set(source_path: Optional[str] = None) -> pd.DataFrame:
     """Read the protein targets of the Papyrus dataset.
 
         :param source_path: folder containing the molecular descriptor datasets
         :return: the set of protein targets in the Papyrus dataset
         """
-    # Load data types
+    # Determine default paths
+    if source_path is not None:
+        os.environ['PYSTOW_HOME'] = os.path.abspath(source_path)
+    source_path = pystow.module('papyrus')
+    source_path = source_path.base.as_posix()
+    # Find the file
     file_mask = os.path.join(source_path, f'*.*_combined_set_protein_targets.tsv*')
     filename = glob.glob(file_mask)
     # Handle WSL ZoneIdentifier files
