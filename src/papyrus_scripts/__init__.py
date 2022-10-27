6ad4ad18faf471e5c77921c95170154385a43353
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2022-10-27 15:59:48 +0200

    Update __init__.py

--- src/papyrus_scripts/__init__.py
+++ src/papyrus_scripts/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """A collection of scripts to handle the Papyrus bioactivity dataset."""
+
+from .download import download_papyrus, remove_papyrus
+from .reader import read_papyrus, read_protein_set, read_protein_descriptors, read_molecular_descriptors
+
+from .matchRCSB import update_rcsb_data, get_matches
+from .preprocess import (keep_organism, keep_accession, keep_type, keep_source,
+                         keep_protein_class, keep_quality, keep_contains, keep_match,
+                         keep_similar, keep_substructure)
+
+from .modelling import qsar, pcm
